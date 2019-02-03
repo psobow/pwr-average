7b8f79874cb6b99b4df99ce18bcd069f7daf0318
Patryk Sobów <38480698+psobow@users.noreply.github.com>
2019-02-03 19:06:28 +0100

    Add files via upload

---
+++ pwr-average.js
@@ -0,0 +1,19 @@
+$('td:contains("2.0")').parent().remove();
+$('td:contains("---")').parent().remove(); 
+$('td:contains("k.cz.")').parent().remove(); 
+
+var sumaECTS=0; 
+var sumaECTSrazyOcena=0;
+
+$('td[title="ocena"]').each(function(){
+	sumaECTSrazyOcena += parseFloat($(this).text()) * parseFloat($(this).next().next().text());
+	sumaECTS += parseFloat($(this).next().next().text());
+	console.log(parseFloat($(this).text()), parseFloat($(this).next().next().text()));
+});
+
+console.log(
+"suma ECTS * Ocena:", sumaECTSrazyOcena,
+"suma ECTS:", sumaECTS,
+"srednia wazona z calych studiow:", sumaECTSrazyOcena/sumaECTS
+);
+
